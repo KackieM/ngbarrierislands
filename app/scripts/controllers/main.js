5d899c7d7bec5244f560e83a7f16d7b8cc950acf
Katherine Mattison <kbmedisto@gmail.com>
2014-08-06 17:21:53 -0400

    got my markers on my islands

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -14,36 +14,56 @@ angular.module('ngbarrierislandsApp')
     		latitude: 32.842271,
     		longitude: -79.860806
     	},
-    	zoom: 11
+    	zoom: 8
     }
 
     $scope.marker = {
     	coords: {
-    		latitude: 45,
-    		longitude: -73
+    		latitude: 32.842271,
+    		longitude: -79.860806
     	}
     }
 
     $scope.markerList = [
     	{
-    		latitude: 45,
-    		longitude: -74
+    		latitude: 32.8007336,
+    		longitude: -79.7646715
     	},
     	{
-    		latitude: 46,
-    		longitude: -75
+    		latitude: 32.940138,
+    		longitude: -79.657388
     	},
     	{
-    		latitude: 47,
-    		longitude: -76
+    		latitude: 32.3164625,
+    		longitude: -80.4945008
     	},
     	{
-    		latitude: 48,
-    		longitude: -77
+    		latitude: 32.6526523,
+    		longitude: -79.9481098
     	},
     	{
-    		latitude: 49,
-    		longitude: -78
+    		latitude: 32.3650467,
+    		longitude: -80.4545612
+    	},
+    	{
+    		latitude: 32.1894928,
+    		longitude: -80.7488113
+    	},
+    	{
+    		latitude: 32.368381,
+    		longitude: -80.5522004
+    	},
+    	{
+    		latitude: 32.565651,
+    		longitude: -80.3089455
+    	},
+    	{
+    		latitude: 32.4924083,
+    		longitude: -80.3801018
+    	},
+    	{
+    		latitude: 32.4837918,
+    		longitude: -80.4028803
     	},
 
 
