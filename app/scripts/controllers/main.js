fe7fcc222f63ca80b433d489367315ad4f0de8a0
Katherine Mattison <kbmedisto@gmail.com>
2014-08-06 16:29:57 -0400

    errors but pushing on with the code

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,9 +2,53 @@
 
 angular.module('ngbarrierislandsApp')
   .controller('MainCtrl', function ($scope) {
+
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+    $scope.map = {
+    	center: {
+    		latitude: 45,
+    		longitude: -73
+    	},
+    	zoom: 8
+    }
+
+    $scope.marker = {
+    	coords: {
+    		latitude: 45,
+    		longitude: -73
+    	}
+    }
+
+    $scope.markerList = [
+    	{
+    		latitude: 45,
+    		longitude: -74
+    	},
+    	{
+    		latitude: 46,
+    		longitude: -75
+    	},
+    	{
+    		latitude: 47,
+    		longitude: -76
+    	},
+    	{
+    		latitude: 48,
+    		longitude: -77
+    	},
+    	{
+    		latitude: 49,
+    		longitude: -78
+    	},
+
+
+
+
+    	]
+
   });
