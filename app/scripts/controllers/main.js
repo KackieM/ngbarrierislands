f8654c358c460eb2a02501fc4563d1eab8deac5a
Katherine Mattison <kbmedisto@gmail.com>
2014-08-07 20:06:22 -0400

    adding in messages to the main js with messages in the main js in order to have the markers go somewhere

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -20,50 +20,61 @@ angular.module('ngbarrierislandsApp')
     $scope.marker = {
     	coords: {
     		latitude: 32.842271,
-    		longitude: -79.860806
+    		longitude: -79.860806,
+    		message: 'marker 1'
     	}
     }
 
     $scope.markerList = [
     	{
     		latitude: 32.8007336,
-    		longitude: -79.7646715
+    		longitude: -79.7646715,
+    		message: 'marker 2'
     	},
     	{
     		latitude: 32.940138,
-    		longitude: -79.657388
+    		longitude: -79.657388,
+    		message: 'marker 3'
     	},
     	{
     		latitude: 32.3164625,
-    		longitude: -80.4945008
+    		longitude: -80.4945008,
+    		message: 'marker 4'
     	},
     	{
     		latitude: 32.6526523,
-    		longitude: -79.9481098
+    		longitude: -79.9481098,
+    		message: 'marker 5'
     	},
     	{
     		latitude: 32.3650467,
-    		longitude: -80.4545612
+    		longitude: -80.4545612,
+    		message: 'marker 6'
     	},
     	{
     		latitude: 32.1894928,
-    		longitude: -80.7488113
+    		longitude: -80.7488113,
+    		message: 'marker 7'
     	},
     	{
     		latitude: 32.368381,
-    		longitude: -80.5522004
+    		longitude: -80.5522004,
+    		message: 'marker 8'
     	},
     	{
     		latitude: 32.565651,
-    		longitude: -80.3089455
+    		longitude: -80.3089455,
+    		message: 'marker 9'
     	},
     	{
     		latitude: 32.4924083,
-    		longitude: -80.3801018
+    		longitude: -80.3801018,
+    		message: 'marker 10'
     	},
     	{
     		latitude: 32.4837918,
-    		longitude: -80.4028803
+    		longitude: -80.4028803,
+    		message: 'marker 11'
     	},
 
 
